fec8e12c5e6727859a8726ab260181b1a6adfcd1
Rodrigo Belli <Rodrigo.Belli@sylvamo.com>
2025-04-04 17:24:48 -0300

    ...

--- src/app/usecase/file/downloadfile.py
+++ src/app/usecase/file/downloadfile.py
@@ -15,13 +15,10 @@ class DownloadFile(UseCase):
         if not file_meta:
             raise FileNotFoundError(f"File with id {file_id} not found.")
 
-        # Baixar os bytes do arquivo do storage
         file_bytes = self.storage.download(file_meta.file_path)
 
-        # Calcular o hash do arquivo baixado
         calculated_hash = self.hash_service.generate(file_bytes)
 
-        # Validar integridade
         if calculated_hash != file_meta.file_hash:
             raise FileIntegrityError(f"Hash mismatch for file {file_id}")
 
